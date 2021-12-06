ffc07fc5a26d497a583acc38a669cde9be35bc47
Ben Zenittini <ben.zenittini@gmail.com>
2021-12-05 20:33:58 -0500

    Ben : Made improvements to day 5 solution.

--- ben/day5/task2.js
+++ ben/day5/task2.js
@@ -1,3 +1,23 @@
+
+// =================
+// Utility Functions
+// -----------------
+
+/**
+ * Generates and returns an incrementing or decrementing array from 'begin' to 'end'.
+ * If 'begin' equals 'end', then generates a repeating array for the given length.
+ */
+function range(begin, end, length) {
+  if      (begin < end) return Array(end - begin + 1).fill(0).map((val, i) => i + begin);
+  else if (begin > end) return Array(begin - end + 1).fill(0).map((val, i) => i + end).reverse();
+  else                  return Array(length).fill(begin);
+}
+
+
+// =================
+// Program Execution
+// -----------------
+
 const fs = require('fs');
 
 const lines = fs.readFileSync('input', 'UTF-8').split(/\r?\n/);
@@ -14,29 +34,21 @@ const board = lines
     };
   })
 
-  // Flatten each line to an array of points (don't judge - you know you love this function)
+  // Flatten each line to an array of points, represented by a string ('x,y')
   .reduce((points, {start, stop}) => {
-    if      (start.x < stop.x) var [xOp, xCondition] = [x => x+1, x => x <= stop.x];
-    else if (start.x > stop.x) var [xOp, xCondition] = [x => x-1, x => x >= stop.x];
-    else                       var [xOp, xCondition] = [x => x,   x => true];
-    if      (start.y < stop.y) var [yOp, yCondition] = [y => y+1, y => y <= stop.y];
-    else if (start.y > stop.y) var [yOp, yCondition] = [y => y-1, y => y >= stop.y];
-    else                       var [yOp, yCondition] = [y => y,   y => true];
+    // Generate range arrays for the X's and Y's. (ex: [3, 4, 5] or [8, 7, 6, 5, 4])
+    const xs = range(start.x, stop.x, Math.abs(start.y - stop.y)+1);
+    const ys = range(start.y, stop.y, Math.abs(start.x - stop.x)+1);
 
-    for (let [x,y] = [start.x,start.y];
-         xCondition(x) && yCondition(y);
-         [x,y] = [xOp(x), yOp(y)]) {
-      points.push({x, y});
-    }
-    return points;
+    // "Zip" xs and ys together into a single string ('x,y')
+    return xs.map((val, i) => `${val},${ys[i]}`).concat(points);
   }, [])
 
   // Record each point on a "board". The board is a coordinate system lookup for the
   // counts to avoid having a billion empty array cells: { 'x,y': count, ... }
-  .reduce((board, {x, y}) => {
-    const key = `${x},${y}`;
-    board[key] = board[key] ? board[key]+1 : 1;
-    return board;
-  }, {});
+  .reduce((board, key) =>
+    Object.assign(board, { [key]: (board[key] ? board[key]+1 : 1) }),
+    {});
 
-console.log(Object.values(board).filter(count => count >= 2).length);
+const count = Object.values(board).filter(count => count >= 2).length;
+console.log(count);
