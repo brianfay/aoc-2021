f60af6bb9408d517c9c77001d6272841e43c2ac1
Sean M Hill <smhill818@gmail.com>
2021-12-06 22:40:41 -0500

    Sean: unsilly day 6 (it's called a... list, you say?)

--- sean/6/2.py
+++ sean/6/2.py
@@ -1,15 +1,13 @@
 # Thatsalottafishies.
 
-fish_counts = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}  # int keys feel kinda weird but they work here
+fish_counts = [0] * 9
 with open('input.txt') as input_file:
     for fish_age in input_file.readline().split(','):
         fish_counts[int(fish_age)] += 1
 
 for _ in range(256):
-    expecting_count = fish_counts[0]
-    for i in range(1, 8 + 1):  # Skip 0 and include 8
-        fish_counts[i - 1] = fish_counts[i]  # Move all the counts down one day
+    expecting_count = fish_counts.pop(0)
     fish_counts[6] += expecting_count  # Fish-mommies beginning maternity leave
-    fish_counts[8] = expecting_count  # New babies
+    fish_counts.append(expecting_count)  # New babies
 
-print(sum(fish_counts.values()))
+print(sum(fish_counts))
