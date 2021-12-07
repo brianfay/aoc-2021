a57f21dce136e5f82387269dbd49000966a2c6ad
Sean M Hill <smhill818@gmail.com>
2021-12-07 08:24:56 -0500

    Sean: concise-ify 6.2 a bit

--- sean/6/2.py
+++ sean/6/2.py
@@ -1,13 +1,12 @@
-# Thatsalottafishies.
+# Concise-ified a bit from part 1. Thatsalottafishies.
 
 fish_counts = [0] * 9
 with open('input.txt') as input_file:
-    for fish_age in input_file.readline().split(','):
-        fish_counts[int(fish_age)] += 1
+    for fish_age in map(int, input_file.readline().split(',')):
+        fish_counts[fish_age] += 1
 
 for _ in range(256):
-    expecting_count = fish_counts.pop(0)
-    fish_counts[6] += expecting_count  # Fish-mommies beginning maternity leave
-    fish_counts.append(expecting_count)  # New babies
+    fish_counts.append(fish_counts.pop(0)) # New babies to the back
+    fish_counts[6] += fish_counts[-1]  # Fish-mommies beginning maternity leave
 
 print(sum(fish_counts))
