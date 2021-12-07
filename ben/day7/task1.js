0facf713fe0b2be46875e72ba6eb78a5b7949d58
Ben Zenittini <ben.zenittini@gmail.com>
2021-12-07 11:33:56 -0500

    Ben : Because we all know less lines equals more better. Day 7 updates.

--- ben/day7/task1.js
+++ ben/day7/task1.js
@@ -1,19 +1,14 @@
 // https://www.youtube.com/watch?v=LDU_Txk06tM&t=29s
 
-let positions = require('fs').readFileSync('input', 'UTF-8')
-  .split(',')
-  .map(p => parseInt(p, 10));
+let positions = require('fs').readFileSync('input', 'UTF-8').split(',').map(p => parseInt(p, 10));
 
 // Any number between the min and max could be valid optimal positions
 let [min, max] = [Math.min(...positions), Math.max(...positions)];
-let fuelCosts = Array(max-min+1).fill(0)
-  // Build up an object of fuel costs for each position (key = position, val = fuel cost)
-  .reduce((posToFuelCost, _, index) => {
-    let fuelCost = positions               // Total fuel equals
-      .map(p => Math.abs(p - (min+index))) // Distance for each crab
-      .reduce((a, b) => a + b);            // Added together
-    return Object.assign(posToFuelCost, { [min+index]: fuelCost });
-  }, {});
+let calculateFuel = (destination) => ((position) => Math.abs(position - destination));
+
+// Calculate an array of fuel costs for each position.
+let fuelCosts = Array(max-min+1).fill(0).map((_, index) =>
+  positions.map(calculateFuel(min+index)).reduce((a, b) => a + b));
 
 // Figure out the most fuel efficient option
-console.log(Math.min(...Object.values(fuelCosts)));
+console.log(Math.min(...fuelCosts));
--- ben/day7/task2.js
+++ ben/day7/task2.js
@@ -1,21 +1,14 @@
 // https://www.youtube.com/watch?v=LDU_Txk06tM&t=29s
 
-let positions = require('fs').readFileSync('input', 'UTF-8')
-  .split(',')
-  .map(p => parseInt(p, 10));
+let positions = require('fs').readFileSync('input', 'UTF-8').split(',').map(p => parseInt(p, 10));
 
 // Any number between the min and max could be valid optimal positions
 let [min, max] = [Math.min(...positions), Math.max(...positions)];
-let fuelCosts = Array(max-min+1).fill(0)
-  // Build up an object of fuel costs for each position (key = position, val = fuel cost)
-  .reduce((posToFuelCost, _, index) => {
-    let fuelCost = positions.map(p => {
-        let distance = Math.abs(p - (min+index))
-        return (distance*distance + distance)/2; // Because math.
-      })
-      .reduce((a, b) => a + b);
-    return Object.assign(posToFuelCost, { [min+index]: fuelCost });
-  }, {});
+let calculateFuel = (destination) => ((position) => (Math.pow(Math.abs(position - destination), 2) + Math.abs(position - destination))/2);
+
+// Calculate an array of fuel costs for each position.
+let fuelCosts = Array(max-min+1).fill(0).map((_, index) =>
+  positions.map(calculateFuel(min+index)).reduce((a, b) => a + b));
 
 // Figure out the most fuel efficient option
-console.log(Math.min(...Object.values(fuelCosts)));
+console.log(Math.min(...fuelCosts));
