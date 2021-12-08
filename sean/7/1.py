1812f001b0f7ebe4e25329a21f882579e40df19c
Sean M Hill <smhill818@gmail.com>
2021-12-07 22:01:29 -0500

    Sean: improve day 7 with math

--- sean/7/1.py
+++ sean/7/1.py
@@ -1,10 +1,16 @@
 # On a hunch, I used the median to get a "middle" position that isn't skewed by outliers.
 # To my surprise, it actually worked! Full disclosure: I tried mean first...
 
-import statistics
+# But the median can be halfway between two numbers, and what do you do then? Is it really enough just to round?
+# This niggling doubt seems to have some truth to it. Reading the mathy comments on the subreddit made me realize I had
+# to check both positions around the mean for part 2, which I'm guessing also applies to the median for part 1.
+# So hopefully this now works for every input! (But it's still just a guess, to be honest.)
+
+import math, statistics  # WOOoOo gettin' mathy!
 
 with open('input.txt') as input_file:
     positions = [int(num) for num in input_file.readline().split(',')]
 
-median_position = round(statistics.median(positions))
-print(sum(abs(position - median_position) for position in positions))
+median = statistics.median(positions)
+positions_to_check = (math.floor(median), math.ceil(median))
+print(min(sum(abs(position - position_to_check) for position in positions) for position_to_check in positions_to_check))
--- sean/7/2.py
+++ sean/7/2.py
@@ -1,13 +1,20 @@
-# "Bah! There's only 2000 or so--let's just check 'em all."--me after failing to find an elegant mathematical solution
+# Bahhh! My first intuition here was this: "Well, outliers seem significant now, so... Try the mean?"
+# And I tried. And tried. The ordinary mean didn't work, so I tried a distance-weighted mean where I modified the
+# weights to be proportional to the total distance to all other points... But at that point I was already most of the
+# way to brute force--so I threw up my hands, put down my abacus, and picked up my club.
 #
-# So I just checked every possible position between the min and max to see which one had the lowest fuel cost...
+# Afterwards, I looked at the subreddit... AND THE MEAN WORKS.
+# You just need to check both numbers around it because of math reasons.
+# I KNEW IT! (Not really.) I HAD A VAGUE FEELING IT MIGHT BE TRUE! (Okay, sort of.) I'LL TAKE IT! (Fine, jeez.)
 #
-# At least I got to use the formula for an arithmetic series!
+# For fuel cost, I used the formula for an arithmetic series.
 # That's the sum of the first n terms of an arithmetic sequence.
 # That's a sequence where the numbers increase by adding a constant value.
 # Interestingly, the constant value doesn't matter for computing the sum! Hum dee dum!
 # I would like to thank Google for interpreting my words, which were not as mathy as those, as those.
 # How cool it would be to actually know math?! Approximatey 3 ↑↑↑↑ 3 cool, I reckon.
+
+import math
 
 with open('input.txt') as input_file:
     positions = [int(num) for num in input_file.readline().split(',')]
@@ -15,4 +22,6 @@ with open('input.txt') as input_file:
 def fuel_distance(a, b):
     return abs(a - b) * (1 + abs(a - b)) / 2  # Arithmetic series
 
-print(min(sum(fuel_distance(start, end) for start in positions) for end in range(min(positions), max(positions))))
+mean = sum(positions) / len(positions)
+positions_to_check = (math.floor(mean), math.ceil(mean))
+print(min(sum(fuel_distance(start, end) for start in positions) for end in positions_to_check))
