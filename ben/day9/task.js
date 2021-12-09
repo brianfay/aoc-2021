c69d90bfbd226e941fbb388034b0bc4a7047e879
Ben Zenittini <ben.zenittini@gmail.com>
2021-12-09 18:16:22 -0500

    Ben : Day 9 working solutions. I am a dumb.

--- ben/day9/task.js
+++ ben/day9/task.js
@@ -1,58 +1,34 @@
 
 const lines = require('fs').readFileSync('input', 'UTF-8').split(/\r?\n/);
 
-printBoardWithMinimasHighlighted();
 runPart1();
 runPart2();
-
-
-// =================
-// Utility Functions
-// -----------------
-
-function isLocalMinimum(data, location, lineLength) {
-    const neighbors = [
-        data[location-1] ?? 9,          // left
-        data[location+1] ?? 9,          // right
-        data[location-lineLength] ?? 9, // up
-        data[location+lineLength] ?? 9  // down
-    ];
-    return data[location] < Math.min(...neighbors);
-}
-
-function printBoardWithMinimasHighlighted() {
-    const result = lines
-        .flatMap(l => l.split(''))
-        .map(p => parseInt(p, 10))
-        .reduce((prev, curr, loc, data) => {
-            prev.push({num: curr, isMinimum: isLocalMinimum(data, loc, lines[0].length)})
-            return prev;
-        }, [])
-        .map(({num, isMinimum}) => isMinimum ? `\x1b[31m${num}\x1b[0m` : num)
-        .reduce((prev, curr) => {
-            if (prev[prev.length-1].length === lines[0].length) {
-                prev.push([]);
-            }
-            prev[prev.length-1].push(curr);
-            return prev;
-        }, [[]])
-        .forEach((line) => console.log(line.join('')));
-}
-
 
 // ======
 // Part 1
 // ------
 
 function runPart1() {
-    const result = lines
-        .flatMap(l => l.split(''))       // Combine into one mega array
-        .map(p => parseInt(p, 10))       // Convert from strings to ints
-        .filter((p,loc,data) => isLocalMinimum(data, loc, lines[0].length)) // Keep only the local minimas
+    const caveSystem = lines.map(l => l.split('').map(p => parseInt(p, 10)));
+
+    const result = caveSystem
+        .flatMap((row, rowIndex) => row.map((height, colIndex) => ({row: rowIndex, col: colIndex, height})))
+        .filter(location => isLocalMinimum(caveSystem, location)) // Keep only the local minimas
+        .map(({height}) => height)
         .reduce((a, b) => a + b + 1, 0); // Sum up the result
 
-    // 564 is too low :\
     console.log("Part 1: " + result);
+}
+
+function isLocalMinimum(caveSystem, {row, col}) {
+    // Won't you be my neighbor?
+    const neighbors = [
+        (caveSystem[row] ?? [])[col-1] ?? 9,
+        (caveSystem[row] ?? [])[col+1] ?? 9,
+        (caveSystem[row-1] ?? [])[col] ?? 9,
+        (caveSystem[row+1] ?? [])[col] ?? 9,
+    ];
+    return caveSystem[row][col] < Math.min(...neighbors);
 }
 
 
@@ -61,4 +37,46 @@ function runPart1() {
 // ------
 
 function runPart2() {
+    const caveSystem = lines.map(l => l.split('').map(p => parseInt(p, 10)));
+
+    // * UNDERWATER FLOOD! *
+    // Water has started spewing out of the floor. It started simultaneously at all the lowest 
+    // points in the cave (height 0), and once all those basins were full, it started spewing
+    // out of non-flooded height 1 locations. This pattern continued until this water-filled
+    // cave filled up with water! The horrors!
+    let basins = [];
+    let remainingPoints = caveSystem
+        .flatMap((row, rowIndex) => row.map((height, colIndex) => ({row: rowIndex, col: colIndex, height})))
+        .filter(({height}) => height !== 9);
+    for (let basinLowPoint = 0; basinLowPoint < 9; basinLowPoint++) {
+        // Find and record all the newly flooded basins
+        let newBasins = remainingPoints.filter(({height}) => height === basinLowPoint)
+            .map(({row, col}) => getBasinPoints(caveSystem, {row, col}, []));
+        basins.push(...newBasins);
+
+        // Update our understanding of the caves by removing all the flooded areas from existence.
+        let usedLocations = basins.flatMap(i => i);
+        remainingPoints = remainingPoints.filter(({row,col}) => !usedLocations.includes(`${row},${col}`));
+    }
+
+    const product = basins.map(b => b.length).sort((a,b) => b-a).slice(0,3).reduce((a,b) => a*b);
+    console.log("Part 2: " + product);
+}
+
+function getBasinPoints(caveSystem, thisLocation, locationsSoFar) {
+    let {row, col} = thisLocation;
+    // If it's a "9" or edge of map, then exit.
+    if (((caveSystem[row] ?? [])[col] ?? 9) === 9) return [];
+    // If it's already been checked, then exit.
+    if (locationsSoFar.includes(`${row},${col}`)) return [];
+
+    // Otherwise, add it and check its neighbors!
+    let newPoints = [];
+    newPoints.push(`${row},${col}`);
+    newPoints.push(...getBasinPoints(caveSystem, {row: row-1, col: col}, [...locationsSoFar, ...newPoints]));
+    newPoints.push(...getBasinPoints(caveSystem, {row: row+1, col: col}, [...locationsSoFar, ...newPoints]));
+    newPoints.push(...getBasinPoints(caveSystem, {row: row, col: col-1}, [...locationsSoFar, ...newPoints]));
+    newPoints.push(...getBasinPoints(caveSystem, {row: row, col: col+1}, [...locationsSoFar, ...newPoints]));
+
+    return newPoints;
 }
