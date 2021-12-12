4ff1dfa0ed47c69355033a926d25b806894023c2
Ben Zenittini <ben.zenittini@gmail.com>
2021-12-11 19:13:56 -0500

    Ben : Renamed directories for ordering in github.

---
+++ ben/day11/task.js
@@ -0,0 +1,23 @@
+
+const lines = require('fs').readFileSync('input', 'UTF-8').split(/\r?\n/);
+
+const closer = { '(':')', '{':'}', '[':']', '<':'>' };
+
+runPart1();
+runPart2();
+
+
+// ======
+// Part 1
+// ------
+
+function runPart1() {
+}
+
+
+// ======
+// Part 2
+// ------
+
+function runPart2() {
+}
